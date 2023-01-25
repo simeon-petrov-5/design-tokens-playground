3298d3076ac439835e789f834e2e764901ffc2f7
Simeon Petrov <s.petrov@blubito.com>
2023-01-25 14:24:43 +0200

    feat: Adjust base tokens; Fix gul tasks to run in series

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Design token playground
 
 WIP
-
+https://marketplace.visualstudio.com/items?itemName=vunguyentuan.vscode-css-variables
 
 ## Resourcess
 - https://www.youtube.com/watch?v=BPtwhJQCstg&t=901s
@@ -12,3 +12,5 @@ WIP
 - https://www.youtube.com/watch?v=map-zXfy6y0
 - https://medium.com/eightshapes-llc/space-in-design-systems-188bcbae0d62
 - https://www.youtube.com/watch?v=eWT394G04Os
+- https://helio.app/definitions/modular-scale/
+- https://www.modularscale.com/?1&rem&1.5
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
-// REQUIRED https://marketplace.visualstudio.com/items?itemName=vunguyentuan.vscode-css-variables
-
-const gulp = require("gulp");
+const { src, dest, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const csso = require('gulp-csso');
+const fs = require('fs');
 
-function buildBaseTokens(callback) {
+function buildBaseTokens() {
   const scssPath = "./tokens/base/base-tokens.scss";
-  gulp
-    .src(scssPath)
-    .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
-    .pipe(gulp.dest("./public/tokens"));
-  callback();
-}
-
-function buildContextTokens(callback) {
-  const scssPath = "./tokens/context/context-tokens.scss";
-  gulp
-    .src(scssPath)
+  return src(scssPath)
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./public/tokens"));
-  callback();
+    .pipe(dest("./dist/tokens"))
 }
 
-function mergeFiles(callback) {
-  const cssPath = "./public/tokens/*.css";
-  gulp
-    .src(cssPath)
+function buildContextTokens() {
+  const scssPath = "./tokens/context/context-tokens.scss";
+  return src(scssPath)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./dist/tokens"))
+}
+
+function mergeCSS() {
+  const cssPath = "./dist/tokens/*.css";
+  return src(cssPath)
     .pipe(concat("tokens.css"))
     .pipe(csso())
-    .pipe(gulp.dest("./public"));
-  callback();
+    .pipe(dest("./dist/"))
+}
+
+function clean() {
+  return new Promise((resolve) => {
+    fs.rmSync('./dist/tokens', { recursive: true, force: true });
+    resolve();
+  })
 }
 
 
-exports.compressCSS = mergeFiles;
-exports.buildCSS = gulp.series(buildBaseTokens, buildContextTokens);
+exports.buildCSS = series(buildBaseTokens, buildContextTokens, mergeCSS, clean);
 
